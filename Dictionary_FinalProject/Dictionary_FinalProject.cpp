7f2f893c5962e4a8dfb65d9ea3e62f8005d35b9b
Amanuel Getachew <agetachew73@gmail.com>
2021-10-17 15:44:01 +0300

    write to file implemented

--- Dictionary_FinalProject/Dictionary_FinalProject.cpp
+++ Dictionary_FinalProject/Dictionary_FinalProject.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
+#include <fstream>
+#include<string>  
 
 using namespace std;
 
+//string enumerations for word types
+string noun = "noun", pronoun = "pron", adjective = "adje", adverb = "adve", preposition = "prep", conjunction = "conj", interjection = "inte";
+
+struct dictionary {
+	char word[50];
+	char pron[60]; //pronunciation
+	char type[4];
+	char meaning[200];
+	bool deleted = false;
+};
+
+
+bool writeToFile(dictionary word, fstream& file) {
+	if (!file.is_open()) {
+		cout << "File did not open correctly!!";
+		return false;
+	}
+	else {
+		file.write((char*)&word, sizeof(word));
+		return true;
+	}
+	
+}
+
+
+
 int main()
 {
-    cout << "Hello World!\n";
+	//test for write to file function
+
+	fstream file("data.bin", ios::binary | ios::out);
+
+	dictionary wordData;
+	strcpy_s(wordData.word, "abebe");
+
+	writeToFile(wordData, file);
+
+	
+
+	
 }
